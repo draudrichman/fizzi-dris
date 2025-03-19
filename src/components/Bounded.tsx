067f4eefdfe338092ea7b17519ff30f285612567
draudrichman <88120992+draudrichman@users.noreply.github.com>
2025-03-19 17:54:40 +0600

    Bounded.tsx error

--- src/components/Bounded.tsx
+++ src/components/Bounded.tsx
@@ -1,25 +1,19 @@
 import clsx from "clsx";
 
 type BoundedProps = {
-    as?: React.ElementType;
-    className?: string;
-    children: React.ReactNode;
+  className?: string;
+  children: React.ReactNode;
 };
 
-export const Bounded = ({
-    as: Comp = "section",
-    className,
-    children,
-    ...restProps
-}: BoundedProps) => {
-    return (
-        <Comp
-            className={clsx("px-4 first:pt-10 md:px-6", className)}
-            {...restProps}
-        >
-            <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
-                {children}
-            </div>
-        </Comp>
-    );
+export const Bounded = ({ className, children, ...restProps }: BoundedProps) => {
+  return (
+    <section
+      className={clsx("px-4 first:pt-10 md:px-6", className)}
+      {...restProps}
+    >
+      <div className="mx-auto flex w-full max-w-7xl flex-col items-center">
+        {children}
+      </div>
+    </section>
+  );
 };
